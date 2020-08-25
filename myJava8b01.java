2a0e766587129c967cf68aa83b085791cd69bad9
nameps <50565475+nameps@users.noreply.github.com>
2020-08-25 18:22:27 +0700

    Add files via upload

---
+++ myJava8b01.java
@@ -0,0 +1,23 @@
+package week807;
+
+public class myJava8b01 {
+	public static void main1(String[]args) {
+		
+		dispLayAddress();
+		System.out.println("First Java application");
+		
+	}
+	
+	private static void dispLayAddress() {
+		System.out.println("XYZ Company");
+		System.out.println("89000 U.S. Hny 14");
+		System.out.println("Crystal Lake, IL 60014");
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+	
+	
+}
